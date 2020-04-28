4b589b4d632a795029e83d9d055adab10c2b5234
manukrishnankt <manukrishnankt@gmail.com>
2020-04-28 20:46:22 +0530

    heat map conscept

--- src/app/mapserver/mapserver.component.ts
+++ src/app/mapserver/mapserver.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import * as esri from 'esri-leaflet';
-
+import 'leaflet.heat/dist/leaflet-heat.js';
+import { addressPoints} from '../../assets/realworld.10000'
 @Component({
   selector: 'app-mapserver',
   templateUrl: './mapserver.component.html',
@@ -9,10 +10,11 @@ import * as esri from 'esri-leaflet';
 })
 export class MapserverComponent implements OnInit {
   map: L.Map;
-  curZoomVal : number = 2.4;
+  curZoomVal : number = 10;
   curMaxZoomVal : number = 18;
   curMinZoomVal : number = 0;
-  initLoaded : L.LatLngExpression = [51.505, -0.09];
+  initLoaded : L.LatLngExpression = [-37.8839,175.3745188667];
+  newAddressPoints: any;
   constructor() { }
   ngOnInit(): void {
     this.map = L.map('map', {
@@ -48,19 +50,19 @@ export class MapserverComponent implements OnInit {
     Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 	    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
     });
-
-
+    this.newAddressPoints = addressPoints.map(function (p) { return [p[0], p[1]]; });
+    const heat1 = L.heatLayer(this.newAddressPoints,{});
     var baseLayers = {
-      "steman": steman,
       "cyclOSM": cyclOSM,
-      "Esri_WorldImagery":Esri_WorldImagery
+      "Esri_WorldImagery":Esri_WorldImagery,
+      "heat":heat1,
+      "steman": steman,
     };
 
     var overlays = {
       "Cities": cities
     };
-
     L.control.layers(baseLayers, overlays).addTo(this.map);
-    
   }
+  
 }
